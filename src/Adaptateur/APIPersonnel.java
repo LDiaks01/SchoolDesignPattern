a1df765934db0fdf1f2390e23911c187f7620cba
tourgismacbook <tourgismacbook@macbook-air-de-tourgi.home>
2023-04-08 16:32:59 +0200

    test

--- src/Adaptateur/APIPersonnel.java
+++ src/Adaptateur/APIPersonnel.java
@@ -6,5 +6,6 @@ import school.management.system.Teacher;
 public interface APIPersonnel {
 	    public void add(Membre personnel);
 	    public void remove(Membre membre);
-	    public void update(Membre personnel);
+	    public void update(Membre personnel); 
+	    
 }
